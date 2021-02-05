d0a1591536f042212a0c71c020412f78aa6acb4b
Blashko Maksym <Blashkoma@gmail.com>
2021-02-05 22:10:08 +0200

    Initial Commit

---
+++ WA_Group84_Blashko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WA_Group84_Blashko
+//
+//  Created by Blashko Maksim on 05.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
